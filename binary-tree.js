cfa755084328ac226b8ded068dcd240b69fe3028
Artsiom Shushkevich <artemshushkevich@live.com>
2016-01-28 22:55:46 +0300

    make first commit

---
+++ binary-tree.js
@@ -0,0 +1,34 @@
+'use strict';
+
+class BinaryTree {
+	constructor() {
+		this.root = null;	
+	}
+
+	insert(data) {
+		if ( this.root == null ){
+		
+		}
+
+	}
+
+	contains(data) {
+
+	}
+
+	remove(data) {
+
+	}
+
+	size() {
+
+	}
+
+	isEmpty() {
+		if ( this.root != null ){
+			return false;
+		} else {
+			return true;
+		}
+	}
+}
