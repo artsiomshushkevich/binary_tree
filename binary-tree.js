3b8edd891fd6a784417a3072133e4c3533f4a054
Artsiom Shushkevich <artemshushkevich@live.com>
2016-01-30 01:20:24 +0300

    make size method

--- binary-tree.js
+++ binary-tree.js
@@ -2,45 +2,76 @@
 
 class BinaryTree {
 	constructor() {
-		this.root = null;	
+		this.root = null;
+		this.counter = 0;
 	}
 
 	insert(data) {
-		this.root = this.insertWithNodeParam(this.root, data);
+		this.root = this.insertUsingRecursion(this.root, data);
 	}
 
-	insertWithNodeParam(node, data) {
+	insertUsingRecursion(node, data) {
 		if  (node == null) {
 			return new Node (data, null, null);
 		} else if (node.data > data) {
-			node.left = this.insertWithNodeParam(node.left, data);
+			node.left = this.insertUsingRecursion(node.left, data);
 		} else {
-			node.right = this.insertWithNodeParam(node.right, data)
+			node.right = this.insertUsingRecursion(node.right, data)
 		}
 		return node;
 	}
 
 	contains(data) {
-		return this.containsWithNodeParam(this.root, data);
+		return this.containsUsingRecursion(this.root, data);
 	}
 
-	containsWithNodeParam(node, data) {
+	containsUsingRecursion(node, data) {
 		if (node == null) {
 			return false;
 		}
 		return node.data == data || 
-			   this.containsWithNodeParam(node.left, data) ||
-			   this.containsWithNodeParam(node.right, data);
-		
-		
+			   this.containsUsingRecursion(node.left, data) ||
+			   this.containsUsingRecursion(node.right, data);
+			
 	}
 
+	
 	remove(data) {
 
 	}
 
+
+
+	removeUsingRecursion(node, data) {
+		/*if (node == null) {
+			return; 
+		}
+		if (node.data > data) {
+			node.left = removeUsingRecursion(node.left, data);
+		} else if */
+	}
+
 	size() {
+		if (this.root == null) {
+			return 0;
+		}
+
+		this.sizeUsingRecursion(this.root, 0);
+
+		var tempCounter = this.counter;
+		this.counter = 0;
+		return tempCounter;
+
+	}
+
+	sizeUsingRecursion(node){
+		if (node != null) {
+			this.counter++;
+			this.sizeUsingRecursion(node.left);
+			this.sizeUsingRecursion(node.right);
+		} 
 		
+
 	}
 
 	isEmpty() {
